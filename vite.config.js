3b0d78d872e3c54ebabd697194043f982b703b19
kouhei <kouhei.basketball7.8@gmail.com>
2024-08-18 13:22:54 +0900

    add:react導入

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'; // Reactを使用する場合のプラグイン
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/ts/index.tsx',   // TypeScriptファイル
+                'resources/sass/app.scss', // Sassファイル
+            ],
             refresh: true,
         }),
+        react(),  // Reactを使用する場合
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name].js',
+                assetFileNames: ({ name }) => {
+                    if (/\.(css)$/.test(name ?? '')) {
+                        return 'css/[name].[ext]';
+                    }
+                    return 'assets/[name].[ext]';
+                },
+            },
+        },
+        outDir: 'public', // 出力ディレクトリをpublicに設定
+        emptyOutDir: false, // 出力ディレクトリを空にしない
+    },
 });
