368299a012f5afd4bce1edbec58af86ead09e204
kouhei <kouhei.basketball7.8@gmail.com>
2024-08-25 14:27:39 +0900

    add:useQueryを使い、タスクを表示させる実装、statusに応じてローダーやメッセージを表示させる実装

--- resources/ts/pages/tasks/index.tsx
+++ resources/ts/pages/tasks/index.tsx
@@ -1,53 +1,65 @@
 import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+type Task = {
+    id: number
+    title: string
+    is_done: boolean
+    created_at: Date
+    updated_at: Date
+}
 
 const TaskPage: React.FC = () => {
+    // useQueryを使うため、useEffectでの記述をコメントアウト
+    /*
+    const [ tasks, setTasks ] = useState<Task[]>([]);
+
+    const getTasks = async (): Promise<void> => {
+        const res = await axios.get<Task[]>('api/tasks');
+        setTasks(res.data);
+    }
+    useEffect(() => {
+        getTasks();
+    }, []);
+    */
+
+    // 第一引数はkeyとなる文字列、第二引数は非同期処理
+    // useQueryから受け取る変数はdataとstatusで、dataにはtaskの一覧が入る
+    // data:tasksと記述し。tasksとして使うことができる
+    const { data:tasks, status } = useQuery('tasks', async (): Promise<Task[]> => {
+        const res = await axios.get<Task[]>('api/tasks');
+        return res.data;
+    })
+
+    if(status === 'loading') {
+        return <div className="loader" />
+    } else if (status === 'error') {
+        return <div className="align-center">データの読み込みに失敗しました。</div>
+    } else if (!tasks || tasks.length <= 0) {
+        return <div className="align-center">登録されたタスクはありません。</div>
+    }
+
     return (
         <>
             <form className="input-form">
                 <div className="inner">
-                    <input type="text" className="input" placeholder="TODOを入力してください。" value="" />
+                    <input type="text" className="input" placeholder="TODOを入力してください。" defaultValue="" />
                     <button className="btn is-primary">追加</button>
                 </div>
             </form>
             <div className="inner">
                 <ul className="task-list">
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>新しいTODO</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <form>
-                            <input type="text" className="input" value="編集中のTODO" />
-                        </form>
-                        <button className="btn">更新</button>
-                    </li>
-                    <li className="done">
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>実行したTODO</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>ゴミ捨て</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>掃除</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            <label className="checkbox-label">
+                                <input type="checkbox" className="checkbox-input" />
+                            </label>
+                            <div><span>{ task.title }</span></div>
+                            <button className="btn is-delete">削除</button>
+                        </li>
+                    ))}
+
                 </ul>
             </div>
         </>
